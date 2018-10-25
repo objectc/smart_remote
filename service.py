ad4186fb5961f9fc2a1e38ffb35837aaaf59b6cb
tab <tab88tab@gmail.com>
2018-10-25 01:49:38 -0400

    change gpio mode

--- service.py
+++ service.py
@@ -1,10 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 import os
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(27, GPIO.IN)
 while True:
     if(GPIO.input(27)):
         os.system("irsend send_once Dyson POWER")
-        break
 time.sleep(1)
