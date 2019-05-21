f85e7bd7ce35ec354cbdc8d329c37e74e16b9e6a
tab <tab88tab@gmail.com>
2019-05-21 00:37:57 -0400

    update

--- service.py
+++ service.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 import os
+from flask import Flask
+from flask_ask import Ask, request, session, question, statement
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(27, GPIO.IN)
 while True:
